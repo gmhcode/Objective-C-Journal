85ffe8fdab1137531a0d3d040c02d30a8494e828
GMHCode <greghughes988@gmail.com>
2018-12-17 13:45:03 -0700

    Initial Commit

---
+++ Journal OBJC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal OBJC
+//
+//  Created by Greg Hughes on 12/17/18.
+//  Copyright © 2018 Greg Hughes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Journal OBJC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Journal OBJC
+//
+//  Created by Greg Hughes on 12/17/18.
+//  Copyright © 2018 Greg Hughes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
